d250146943daf3088a3c199fea0f38d112ba1b67
Carlos Alberto Vergara Marrugo <cvergara@smdigital.com.co>
2014-10-28 17:07:44 -0500

    Snippet for Response class

--- demo.php
+++ demo.php
@@ -1,2 +1,10 @@
 <?php
-{{ Form::open(['name'=>'', 'id'=>'', 'url'=>'foo/bar', 'method'=>'post']) }}
+return Response::view('response')->header('Content-type', type);
+return Response::make('content')->withCookie($wshit);
+return Response::json(['key'=>'value']);
+return Response::json(['key'=>'value'])->setCallback(Input::get('callBack'));
+return Response::download('asdasdasd', name, headers);
+
+Response::macro('name', function(value){
+        return Response::make(value, 200);
+        });
